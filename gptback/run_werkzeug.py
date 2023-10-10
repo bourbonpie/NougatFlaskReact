bd1eae62e47a3cf029cd033098acd463207e4afb
Javier Ramirez <jrstst40@gmail.com>
2023-10-10 09:33:25 -0700

    delete py files

--- gptback/run_werkzeug.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.serving import run_simple
-from app import application
-
-if __name__ == '__main__':
-    run_simple('localhost', 3400, application, use_reloader=True, use_evalex=True, use_debugger=True)
